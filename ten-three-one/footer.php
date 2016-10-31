12680832016f2a125ed46a94196948854670507e
stanimeredith <stani.instructor@gmail.com>
2016-10-31 11:38:52 -0700

    Add files via upload

---
+++ ten-three-one/footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+        <cite>
+        &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">nookhouses </a>. All rights reserved.
+        </cite>
+</div>
+<?php wp_footer(); ?>
+ </div>
+ </body>
+ </html>
